04952fe478f1128da587cd95bfa99e0a7c01ae51
Eric <erikasmockus@gmail.com>
2023-03-01 19:34:05 +0200

    added owner index page

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Owner;
 use Illuminate\Http\Request;
 
 class OwnerController extends Controller
 {
     public function index()
     {
-        return view('home');
+        return view('dashboard.owners.index', [
+            'owners' => Owner::all()
+        ]);
     }
 }
---
+++ resources/views/dashboard/owners/index.blade.php
@@ -0,0 +1,75 @@
+<x-layout>
+    <x-dashboard heading="Manage owners">
+        <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6
+          lg:px-8">
+                    <div class="mt-2 mb-6">
+                        <a href="/dashboard/owners/create"
+                            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                            New owner
+                        </a>
+
+                    </div>
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-100">
+                                <tr>
+                                    <th class="px-6 py-4 whitespace-nowrap">
+                                        <div class="flex items-center">
+                                            <div class="text-sm text-black-900 font-bold">
+                                                Name
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <th class="px-6 py-4 whitespace-nowrap">
+                                        <div class="flex items-center">
+                                            <div class="text-sm text-black-900 font-bold">
+                                                Surname
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach ($owners as $owner)
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    {{ $owner->name }}
+                                                </div>
+                                            </div>
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    {{ $owner->surname }}
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="/dashboard/owners/{{ $owner->id }}/edit"
+                                                class="text-blue-500 hover:text-blue-600">Edit</a>
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <form method="POST" action="/dashboard/owners/{{ $owner->id }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="text-xs text-red-400">Delete</button>
+                                            </form>
+                                        </td>
+                                @endforeach
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </x-dashboard>
+</x-layout>
